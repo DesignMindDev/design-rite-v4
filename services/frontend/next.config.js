d16340e7494e34daf12725440e28e544d528cd5d
DesignMindDev <zclient@design-rite.com>
2025-07-17 09:33:57 -0400

    corrected frontend components

--- services/frontend/next.config.js
+++ services/frontend/next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    serverActions: true,
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
