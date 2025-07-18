e47a215ca3839d8a47e0f574080f16cb679fcbe6
DesignMindDev <zclient@design-rite.com>
2025-07-18 09:36:04 -0400

    Fix date-fns version conflict - downgrade to 3.6.0 for compatibility

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
-  eslint: {
+    eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
