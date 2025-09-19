6d9c1dbff62f28959503f6ed8d8cb3608f5857cd
DesignMindDev <zclient@design-rite.com>
2025-09-18 21:53:35 -0400

    Update page.tsx

--- app/education/page.tsx
+++ app/education/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import UnifiedNavigation from '../components/UnifiedNavigation';
 
 export default function EducationHealthcarePage() {
